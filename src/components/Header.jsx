a8ce17479d992ae7f7d4efd17c96a43d39c3d526
Victor Swoboda Neto <32025701+victorsn99@users.noreply.github.com>
2023-10-17 16:43:55 -0300

    Melhoria Resposividade Disp. Móveis

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,8 @@ export function Header() {
     return(
         <header className={styles.header}>
             <img className={styles.igniteLogo} src={igniteLogo} alt="Logotipo do Ignite" />
-            <img className={styles.avatar} src='https://github.com/victorsn99.png'/>
-            <strong className={styles.userName}>Victor Swoboda</strong>
+            {/* <img className={styles.avatar} src='https://github.com/victorsn99.png'/>
+            <strong className={styles.userName}>Victor Swoboda</strong> */}
         </header>
     ) 
 }
